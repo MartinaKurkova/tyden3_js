3ef593074170e4d71c6291bd9c423c79039eb18f
Martina Kurková <martinanoha@seznam.cz>
2024-10-07 18:45:40 +0200

    ockovani

---
+++ script.js
@@ -0,0 +1,5 @@
+//ockovani
+const jmeno = prompt ("Zadejte své jméno a příjmení");
+const vek = Number (prompt ("Zadejte svůj věk"));
+
+document.body.innerHTML = "<p>" + jmeno + ", věk: " + vek + "</p>";
