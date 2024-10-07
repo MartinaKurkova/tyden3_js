1bba5f97709a16edecb11d40f4e0e86d86801699
Martina Kurková <martinanoha@seznam.cz>
2024-10-07 18:59:01 +0200

    koment

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //ockovani
-//const jmeno = prompt ("Zadejte své jméno a příjmení");
-//const vek = Number (prompt ("Zadejte svůj věk"));
+/*const jmeno = prompt ("Zadejte své jméno a příjmení");
+const vek = Number (prompt ("Zadejte svůj věk"));
 
-//document.body.innerHTML = "<p>" + jmeno + ", věk: " + vek + "</p>";
+document.body.innerHTML = "<p>" + jmeno + ", věk: " + vek + "</p>";
 
 //vyplata
 const hodinovka = Number (prompt ("Zadejte svou hodinovou mzdu v Kč"));
@@ -11,7 +11,7 @@ const dny = 21;
 const mzda = Number (hodinovka * hodiny * dny);
 
 document.body.innerHTML = "<p>Vaše měsíční hrubá hodinová mzda je " + mzda + " Kč.</p>";
-
+*/
 
 //vyplata 2
 const hodinovka = Number (prompt ("Zadejte svou hodinovou mzdu v Kč"));
