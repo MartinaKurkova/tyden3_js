d748fdfbcbce9723e3fb37a21ee0f82fc702f879
Martina Kurková <martinanoha@seznam.cz>
2024-10-07 19:27:16 +0200

    kniha

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const dny = 21;
 const mzda = Number (hodinovka * hodiny * dny);
 
 document.body.innerHTML = "<p>Vaše měsíční hrubá hodinová mzda je " + mzda + " Kč.</p>";
-*/
+
 
 //vyplata 2
 const hodinovka = Number (prompt ("Zadejte svou hodinovou mzdu v Kč"));
@@ -20,3 +20,19 @@ const dny = Number (prompt ("Zadejte počet odpracovaných dnů v měsíci"));
 const mzda = Number (hodinovka * hodiny * dny);
 
 document.body.innerHTML = "<p>Vaše měsíční hrubá hodinová mzda je " + mzda + " Kč.</p>";
+*/
+
+const nazev = "Kniha 1";
+const autor = "Michal Kučera";
+const cena = 450;
+const naSklade = true;
+
+const kniha = {
+    nazev: "Kniha 1",
+    autor: "Michal Kučera",
+    cena: 450,
+    naSklade: true,
+}
+
+document.body.innerHTML += "<p>Kniha: " + nazev + ", autor: " + autor + "</p>";
+document.body.innerHTML += "<p>Kniha: " + kniha.nazev + ", autor: " + kniha.autor + "</p>";
